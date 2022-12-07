f1711c3a7131f806017da23c023bd672efed9c45
Carlos Couto <2165764+ThunderSmotch@users.noreply.github.com>
2022-12-07 21:37:59 +0000

    Update which-key.lua

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,4 +1,9 @@
 require("which-key").setup {
+	plugins = {
+		spelling = {
+			enabled = true, -- enable the spellchecking menu
+		}
+	}
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
