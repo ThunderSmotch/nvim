f52984314a1651e2fd7eabf09f398cd50390bcd8
Carlos Couto <2165764+ThunderSmotch@users.noreply.github.com>
2022-12-07 21:37:53 +0000

    Update keymaps.lua

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,15 +1,31 @@
 -- Set <Leader> key to backslash
 vim.g.mapleader = '\\'
 
-vim.keymap.set("n", "<Leader>sv", "<Cmd>source $MYVIMRC<CR>", 
-	{desc = "Reload Vim config from scratch."})
+vim.keymap.set("n", "<Leader>sv", "<Cmd>source $MYVIMRC<CR>", {desc = "Reload Vim config from scratch"})
 
 -- Write and quit if possible/applicable, force quit otherwise
-vim.keymap.set('n', '<Leader>q',
+vim.keymap.set("n", "<Leader>q",
 	function()
-		local success, result = pcall(vim.cmd, 'wq')
+		local success, result = pcall(vim.cmd, "wq")
     	if not success then
-      		vim.cmd('q!')
+      		vim.cmd("q!")
     	end
   	end,
-	{desc = 'Write and quit if possible/applicable, force quit otherwise.'})
+	{desc = "Write and quit if possible/applicable, force quit otherwise"})
+
+vim.keymap.set("n", "<Leader>w", "<Cmd>write<CR>", {desc="Write file"})
+vim.keymap.set("n", "<C-z>", "u", {desc="Undo previous change"})
+
+
+-- Moving Lines
+vim.keymap.set("n", "<S-Up>", "<Cmd>m-2<CR>", {desc="Move current line up"})
+vim.keymap.set("n", "<S-Down>", "<Cmd>m+<CR>", {desc="Move current line down"})
+
+vim.keymap.set("i", "<S-Up>", "<Cmd>m-2<CR>", {desc="Move current line up"})
+vim.keymap.set("i", "<S-Down>", "<Cmd>m+<CR>", {desc="Move current line down"})
+
+-- 1 oine
+-- 2
+-- 3
+-- 4
+-- 5
